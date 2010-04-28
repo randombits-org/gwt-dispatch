9e00b2fc4294d89b2963906428528d7508ab8223
David Peterson <david@randombits.org>
2010-04-28 14:33:53 +1000

    * Added default 'SimpleDispatch' class for non DI implementations.

--- src/main/java/net/customware/gwt/dispatch/server/spring/SpringDispatch.java
+++ src/main/java/net/customware/gwt/dispatch/server/spring/SpringDispatch.java
@@ -1,24 +1,21 @@
 package net.customware.gwt.dispatch.server.spring;
 
-import net.customware.gwt.dispatch.server.AbstractDispatch;
 import net.customware.gwt.dispatch.server.ActionHandlerRegistry;
+import net.customware.gwt.dispatch.server.SimpleDispatch;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * A simple extension of {@link SimpleDispatch} with Spring annotations.
+ * 
+ * @author David Peterson
+ */
 @Component
-public class SpringDispatch extends AbstractDispatch {
-
-    private final ActionHandlerRegistry handlerRegistry;
+public class SpringDispatch extends SimpleDispatch {
 
     @Autowired
     public SpringDispatch( ActionHandlerRegistry handlerRegistry ) {
-        this.handlerRegistry = handlerRegistry;
+        super( handlerRegistry );
     }
-
-    @Override
-    protected ActionHandlerRegistry getHandlerRegistry() {
-        return handlerRegistry;
-    }
-
 }
