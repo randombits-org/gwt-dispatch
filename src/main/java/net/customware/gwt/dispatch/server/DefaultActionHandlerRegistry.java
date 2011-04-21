99cc21516665d1b731c56a765d4afd59d2401f36
Robert Munteanu <robert.munteanu@gmail.com>
2011-04-22 00:16:28 +0300

    Make the DefaultActionHandlerRegistry easier to use with Spring
    Affects #26: Remove need for getActionType() in Action Handlers

--- src/main/java/net/customware/gwt/dispatch/server/DefaultActionHandlerRegistry.java
+++ src/main/java/net/customware/gwt/dispatch/server/DefaultActionHandlerRegistry.java
@@ -1,5 +1,6 @@
 package net.customware.gwt.dispatch.server;
 
+import java.util.List;
 import java.util.Map;
 
 import net.customware.gwt.dispatch.shared.Action;
@@ -26,6 +27,19 @@ public class DefaultActionHandlerRegistry implements InstanceActionHandlerRegist
 
     public <A extends Action<R>, R extends Result> ActionHandler<A, R> findHandler( A action ) {
         return (net.customware.gwt.dispatch.server.ActionHandler<A, R> ) handlers.get( action.getClass() );
+    }
+    
+    /**
+     * Sets the specified <tt>actionHandlers</tt> as the only action handler held by this registry instance
+     * 
+     * @param actionHandlers the list of action handler to set
+     */
+    public void setActionHandlers(List<ActionHandler<?,?>> actionHandlers) {
+        
+        clearHandlers();
+        
+        for ( ActionHandler<?,?> actionHandler : actionHandlers )
+            addHandler(actionHandler);
     }
 
     public void clearHandlers() {
